3267459ef0484148a787392f37ae0ca6b9386ac9
Ronan Silva <ronansilva755@gmail.com>
2023-07-30 16:07:26 -0300

    Criação do painel de pontuação

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const elemento = document.querySelector('#contador')
+let contador = 0;
+
 const jump = () => {
     mario.classList.add('jump')
     setTimeout(() => {
@@ -8,25 +11,27 @@ const jump = () => {
 
 }
 
-
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    elemento.innerHTML = `<p>${contador++}</p>`;
 
     if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
+
         pipe.style.animation = 'none';
         pipe.style.left = '110px';
         
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
         
-        mario.src = './img/game-over.png'
-        mario.style.width ='75px'
-        mario.style.marginLeft ='50px'
+        mario.src = './img/game-over.png';
+        mario.style.width ='75px';
+        mario.style.marginLeft ='50px';
 
-        clearInterval(loop)
+        clearInterval(loop);
     }
+
 }, 10);
 
 document.addEventListener('keydown', jump)
