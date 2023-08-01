389b60cbbd6d56696afdcc1130d8164a31d64c80
Ronan Silva <ronansilva755@gmail.com>
2023-08-01 12:09:42 -0300

    Update
    Habilitando o uso do mouse e de telas para jogar

--- js/script.js
+++ js/script.js
@@ -11,25 +11,26 @@ const jump = () => {
 
 }
 
-const loop = setInterval(() => {
-
+var loop = setInterval(() => {
+    const x = 0;
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     elemento.innerHTML = `<p>${contador++}</p>`;
 
-    if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
+        if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
 
-        pipe.style.animation = 'none';
-        pipe.style.left = '110px';
-        
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-        
-        mario.src = './img/game-over.png';
-        mario.style.width ='75px';
-        mario.style.marginLeft ='50px';
+            pipe.style.animation = 'none';
+            pipe.style.left = '110px';
+            
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+            
+            mario.src = './img/game-over.png';
+            mario.style.width ='75px';
+            mario.style.marginLeft ='50px';
 
-        clearInterval(loop);
+            clearInterval(loop)
+
     }
 
 }, 10);
