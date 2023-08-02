da8c04f6732ccce1b7667dadfd63c7f08c90d932
Ronan Silva <ronansilva755@gmail.com>
2023-08-01 21:29:58 -0300

    Retornando antes do bug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const placar = document.querySelector('#contador')
+let contador =0;
 const jump = () => {
     mario.classList.add('jump')
     setTimeout(() => {
@@ -13,32 +15,21 @@ const loop = setInterval(() => {
     const x = 0;
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    placar.innerHTML = `<p>${contador++}</p>`;
 
     if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
         pipe.style.animation = 'none';
         pipe.style.left = '110px';
-        
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
-        
+
         mario.src = './img/game-over.png'
-        mario.style.width ='75px'
-        mario.style.marginLeft ='50px'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
 
         clearInterval(loop)
     }
 }, 10);
-
-
-// const reset = () =>{
-//     pipe.style.animation = 'pipe-animation 1.5s infinite linear';
-//     pipe.style.left =//Colocar o certo
-
-//     mario.style.animation = '' //olhar qual a condição certa
-//     mario.style.bottom ='0px'
-
-//     mario.src = '../img/mario.gif';
-   
-// }
 
 document.addEventListener('keydown', jump);
