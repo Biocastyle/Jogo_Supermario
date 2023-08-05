fb2c01d2260c92cfffb608625f4272bee3a9dd26
Ronan Silva <ronansilva755@gmail.com>
2023-08-05 11:20:18 -0300

    Melhorias no jogo
    Criação do menu game over com reset//Melhoria no design

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const dplacar = document.querySelector('.contador')
+const gover = document.querySelector('.gameover')
 const placar = document.querySelector('#contador')
+const placar_over = document.querySelector('#contador_over')
+
 let contador =0;
+
 const jump = () => {
     mario.classList.add('jump')
     setTimeout(() => {
@@ -10,26 +15,51 @@ const jump = () => {
 
 }
 
+const reset =() => {
+        pipe.style.animation = 'pipe-animation 1.5s infinite linear';
+        pipe.style.left = '';
 
-const loop = setInterval(() => {
-    const x = 0;
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-    placar.innerHTML = `<p>${contador++}</p>`;
+        mario.style.animation = '';
+        mario.style.bottom = ``;
 
-    if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
-        pipe.style.animation = 'none';
-        pipe.style.left = '110px';
-
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = './img/game-over.png'
-        mario.style.width = '75px'
+        mario.src = './img/mario.gif'
+        mario.style.width = '140px'
         mario.style.marginLeft = '50px'
+        placar_over.innerHTML = `<p>${contador++}</p>`;
 
-        clearInterval(loop)
+        dplacar.style.display = 'block'
+        gover.style.display ='none'
+        
+        contador = 0;
+
     }
-}, 10);
 
-document.addEventListener('keydown', jump);
+const loop = ()=> {
+    const eng = setInterval(() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+        placar.innerHTML = `<p>${contador++}</p>`;
+        
+        if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 100) {
+            pipe.style.animation = 'none';
+            pipe.style.left = '110px';
+    
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+    
+            mario.src = './img/game-over.png'
+            mario.style.width = '75px'
+            mario.style.marginLeft = '50px'
+    
+            dplacar.style.display = 'none'
+            gover.style.display ='block'
+            placar_over.innerHTML = `<p>${contador}</p>`;
+    
+            clearInterval(eng);
+        }
+    }, 10);
+
+}
+
+document.addEventListener('load',loop());
+document.addEventListener('keydown', jump());
